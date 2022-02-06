fb547e0447ccfa6ab4a552f6d07fefc46cf02e89
Arian Sharifi <ariansharifi99@gmail.com>
2022-02-06 17:23:46 +0100

    finere hovedprogram

--- oblig1/Dataklynge.java
+++ oblig1/Dataklynge.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
 public class Dataklynge {
-    private ArrayList<Rack> racks = new ArrayList<Rack>();
+    private ArrayList<Dataklynge.Rack> racks = new ArrayList<Dataklynge.Rack>();
 
     public void leggInnNode(Node nodeObj){
-        for(Rack rackObj: racks){
+        for(Dataklynge.Rack rackObj: racks){
             if(rackObj.rackErFull() == false){
                 rackObj.leggInnNode(nodeObj);
                 return;
             }
         }
-        Rack nyRackObj = new Rack();
+        Dataklynge.Rack nyRackObj = new Rack();
         racks.add(nyRackObj);
         racks.get(racks.size()-1).leggInnNode(nodeObj);
     }   
@@ -18,7 +18,7 @@ public class Dataklynge {
     public int antProsessorer(){
         int antProsessorer = 0; 
         
-        for(Rack rackObj: racks){
+        for(Dataklynge.Rack rackObj: racks){
             int rackAntProsessor = rackObj.antProsessorer();
             antProsessorer += rackAntProsessor;
         }
@@ -28,7 +28,7 @@ public class Dataklynge {
     public int noderMedNokMinne(int paakrevdMinne){
         int noderMedMinne = 0;
 
-        for(Rack rackObj: racks){
+        for(Dataklynge.Rack rackObj: racks){
             int nodeObjMinne = rackObj.noderMedNokMinne(paakrevdMinne);
             noderMedMinne += nodeObjMinne;
         }
@@ -38,4 +38,53 @@ public class Dataklynge {
     public int antallRacks(){
         return racks.size();
     }
+
+    private class Rack{
+        private Node[] noder = new Node[12];
+        private int nodeIRack = 0;
+
+        public void leggInnNode(Node nodeObj){
+            for(int i = 0; i < noder.length; i++) {
+                if(noder[i] == null) {
+                    noder[i] = nodeObj;
+                    nodeIRack++;
+                    return;
+                }
+            }
+        }
+
+        public int antProsessorer(){
+            int antProsessorer = 0;
+            
+            for (Node nodeObj: noder){
+                if (nodeObj != null){
+                    int nodeAntProsessor = nodeObj.antProsessorer();
+                    antProsessorer += nodeAntProsessor;
+                }
+            }
+            return antProsessorer;
+        }
+
+        public int noderMedNokMinne(int paakrevdMinne){
+            int noderMedMinne = 0;
+
+            for(Node nodeObj: noder){
+                if (nodeObj != null){
+                    int nodeMinne = nodeObj.antMinne();
+                    if (paakrevdMinne <= nodeMinne){
+                        noderMedMinne++;
+                    }
+                }
+            }
+            return noderMedMinne;
+        }
+
+        public boolean rackErFull(){
+            if (nodeIRack == 12){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }   
 }
--- oblig1/Hovedprogram.java
+++ oblig1/Hovedprogram.java
@@ -1,67 +1,53 @@
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.file.InvalidPathException;
 import java.util.Scanner;
 
 public class Hovedprogram {
-    private String objektId;
-    private File fil;
-    private Dataklynge dataklynge = new Dataklynge();
-    
-    public Hovedprogram(String objektId, String filPath){
-        this.objektId = objektId;
-        try{
-            URL path = Hovedprogram.class.getResource(filPath);
-            fil = new File(path.getFile());
-        }
-        catch(InvalidPathException | NullPointerException ex){
-            System.out.println(objektId + ": Filen du har oppgitt finnes ikke eller er ikke lesbar");
-            System.exit(0);
-        }
-    }
-    
-    public void lesFil(){
+                
+    public static Dataklynge lesFil(String objektId, String filNavn){
+
+        Dataklynge dataklynge = new Dataklynge();
         Scanner leser = null;
         try{
-            leser = new Scanner(fil);
-            while(leser.hasNextInt()){
+            leser = new Scanner(new File(filNavn));
+
+            while (leser.hasNextInt()) {
                 int antNoder = leser.nextInt();
                 int prosessorer = leser.nextInt();
                 int minne = leser.nextInt();
 
-                if (minne > 4096) throw new IOException("minne");
-                if (prosessorer > 16) throw new IOException("prosessorer");
-                
-                else{
-                    for(int i = 0; i < antNoder; i++){
-                        dataklynge.leggInnNode(new Node(minne, prosessorer));
-                    }
+                if (minne > 4096) throw new IOException("Minne kan ikke vaere hoeyere enn 4096. Minne er " + minne);
+                if (prosessorer > 16) throw new IOException("For mange prossesorer");
+
+                for(int i = 0; i < antNoder; i++) {
+                    dataklynge.leggInnNode(new Node(minne, prosessorer));
                 }
             }
         }
-        catch(IOException e){
+        
+        catch(IOException e) {
             System.out.println(objektId + ": " + "Ugyldig node format i text fil.");
             System.out.println("Ugyldig antall " + e.getMessage());
             System.exit(0);
         }
+
         finally{
-            if(leser != null){
+            if(leser != null) {
                 leser.close();
             }
         }
+
+        return dataklynge;
     }
 
     public static void main(String[] args) {
-        // Hovedprogram dataklynge4 = new Hovedprogram("dataklynge4", "dataklynge4.txt");
-        // dataklynge4.lesFil();
-        Hovedprogram saga = new Hovedprogram("saga", "dataklynge.txt");
-        saga.lesFil();
-        System.out.println(saga.dataklynge.noderMedNokMinne(128));
-        System.out.println(saga.dataklynge.noderMedNokMinne(512));
-        System.out.println(saga.dataklynge.noderMedNokMinne(1024));
-        System.out.println(saga.dataklynge.antProsessorer());
-        System.out.println(saga.dataklynge.antallRacks());
+        Dataklynge dataklynge4 = lesFil("dataklynge4", "dataklynge4.txt");
 
+        Dataklynge saga = lesFil("saga", "dataklynge.txt");
+        System.out.println(saga.noderMedNokMinne(128));
+        System.out.println(saga.noderMedNokMinne(512));
+        System.out.println(saga.noderMedNokMinne(1024));
+        System.out.println(saga.antProsessorer());
+        System.out.println(saga.antallRacks());
     }
 }
